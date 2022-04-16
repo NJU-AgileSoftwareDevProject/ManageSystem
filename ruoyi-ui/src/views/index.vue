e218521f859d0221d620640a1dedfbf27fcd8bff
Akihi <1262540030@qq.com>
2022-04-16 16:19:44 +0800

    views

---
+++ ruoyi-ui/src/views/index.vue
@@ -0,0 +1,140 @@
+<template>
+  <div class="app-container home">
+
+    <el-row :gutter="20">
+      <el-col :sm="24" :lg="12" style="padding-left: 20px">
+        <h2>后台管理框架</h2>
+        <p>
+          NJUSE - 21级硕士研究生敏捷软件开发课程 - 大作业
+        </p>
+        <p>
+          <!-- <b>当前版本:</b> <span>v{{ version }}</span> -->
+        </p>
+        <p>
+          包含用户管理、系统监控两大功能
+        </p>
+        <p>
+          <el-button
+            type="primary"
+            size="mini"
+            icon="el-icon-cloudy"
+            plain
+            @click="goTarget('https://github.com/NJU-AgileSoftwareDevProject/ManageSystem')"
+            >访问码云</el-button
+          >
+          <el-button
+            size="mini"
+            icon="el-icon-s-home"
+            plain
+            @click="goTarget('http://172.19.240.185:1024')"
+            >访问主页</el-button
+          >
+        </p>
+      </el-col>
+
+      <el-col :sm="24" :lg="12" style="padding-left: 50px">
+        <el-row>
+          <el-col :span="12">
+            <h2>小组成员</h2>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="6">
+            
+            <ul>
+              <li>陈氢</li>
+              <li>李瑞</li>
+              <li>王文宇</li>
+              <li>张仕杰</li>
+            </ul>
+          </el-col>
+         
+        </el-row>
+      </el-col>
+    </el-row>
+    
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.8.1",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.home {
+  blockquote {
+    padding: 10px 20px;
+    margin: 0 0 20px;
+    font-size: 17.5px;
+    border-left: 5px solid #eee;
+  }
+  hr {
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border: 0;
+    border-top: 1px solid #eee;
+  }
+  .col-item {
+    margin-bottom: 20px;
+  }
+
+  ul {
+    padding: 0;
+    margin: 0;
+  }
+
+  font-family: "open sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 13px;
+  color: #676a6c;
+  overflow-x: hidden;
+
+  ul {
+    list-style-type: none;
+  }
+
+  h4 {
+    margin-top: 0px;
+  }
+
+  h2 {
+    margin-top: 10px;
+    font-size: 26px;
+    font-weight: 100;
+  }
+
+  p {
+    margin-top: 10px;
+
+    b {
+      font-weight: 700;
+    }
+  }
+
+  .update-log {
+    ol {
+      display: block;
+      list-style-type: decimal;
+      margin-block-start: 1em;
+      margin-block-end: 1em;
+      margin-inline-start: 0;
+      margin-inline-end: 0;
+      padding-inline-start: 40px;
+    }
+  }
+}
+</style>
+
